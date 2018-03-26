33e725edf27388d4cced56aaa8d0b726953376ee
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 22:43:36 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ class Game
     [1,4,7],
     [2,5,8],
     [0,4,8],
-    [2,4,6]
+    [6,4,2]
   ]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
@@ -16,22 +16,15 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
   end
-  def current_player
-    @board.turn_count % 2 == 0 ? @player_1 : @player_2
-  end
+
   def over?
     won? || draw?
   end
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]] &&
-      @board.taken?(combo[0]+1)
-    end
+
+  def current_player
+    @board.turn_count % 2 == 0 ? @player_1 : @player_2
   end
-  def draw?
-    @board.full? && !won?
-  end
+
   def winner
     if winning_combo = won?
       @winner = @board.cells[winning_combo.first]
@@ -52,6 +45,7 @@ class Game
       puts "\n\n"
     end
   end
+
   def play
     while !over?
       turn
@@ -61,5 +55,17 @@ class Game
     elsif draw?
       puts "Cat's Game!"
     end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board.cells[combo[0]] == @board.cells[combo[1]] &&
+      @board.cells[combo[1]] == @board.cells[combo[2]] &&
+      @board.taken?(combo[0]+1)
+    end
+  end
+
+  def draw?
+    @board.full? && !won?
   end
 end
