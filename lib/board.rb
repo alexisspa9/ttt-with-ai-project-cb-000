e72fa376ce08087521ac3050f702b3a09ca27aa3
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 22:04:30 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -11,7 +11,10 @@ class Board
   end
   def board
     puts " #{@cells[0]} , #{@cells[1]} , #{@cells[2]} "
+    puts "-----------"
     puts " #{@cells[3]} , #{@cells[4]} , #{@cells[5]} "
+    puts "-----------"
+
     puts " #{@cells[6]} , #{@cells[7]} , #{@cells[8]} " 
   end
 
