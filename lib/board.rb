4a33634d2359c72ce97b3d1bad3a57fa53c846a9
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 22:04:11 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -9,5 +9,10 @@ class Board
     @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
   end
+  def board
+    puts " #{@cells[0]} , #{@cells[1]} , #{@cells[2]} "
+    puts " #{@cells[3]} , #{@cells[4]} , #{@cells[5]} "
+    puts " #{@cells[6]} , #{@cells[7]} , #{@cells[8]} " 
+  end
 
 end
